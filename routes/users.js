2c32be8e114d17d3d165a4f07da1d2c5d17d3023
prems021 <premji.netcom@gmail.com>
2017-04-23 21:00:36 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,34 @@ router.get('/', function(req, res, next) {
   });
 });
 
+var Sequelize = require('Sequelize');
+
+ var sequelize = new Sequelize('mysql://u519997597_admin:arshavin021@mysql.hostinger.in:3306/u519997597_ks');
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+
+
+
+
+router.get('/create_t1', (request: Request, response: Response) => {
+    var User = sequelize.import(__dirname + "/models/tas_users")
+  
+  User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    USERNAME: 'John',
+    PASSWORD: 'Hancock'
+  });
+});
+
+});
+
 module.exports = router;
